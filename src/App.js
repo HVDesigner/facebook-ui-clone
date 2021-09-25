42e95cb78bc107aafc34eecf7f337576c718cf36
HoangNgocViet <hoangngocviet@HoangNgocViets-iMac.local>
2021-09-25 16:12:30 +0700

    at home 4:12PM

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import Friend from "./svg/friend-fill";
+import React from "react";
+import HomeOutline from "./svg/home-outline";
+// import HomeFill from "./svg/home-fill";
+import FriendOutline from "./svg/friend-outline";
+// import FriendFill from "./svg/friend-fill";
+import PageOutline from "./svg/page-outline";
+// import PageFill from "./svg/page-fill";
+import WatchOutline from "./svg/watch-outline";
+// import WatchFill from "./svg/watch-fill";
+import GroupOutline from "./svg/group-outline";
+// import GroupFill from "./svg/group-fill";
 import "./App.css";
 
-function App() {
+function App() {  
+  const [list, _] = React.useState([
+    {
+      icon: HomeOutline,
+    },
+    {
+      icon: FriendOutline,
+    },
+    {
+      icon: PageOutline,
+    },
+    {
+      icon: WatchOutline,
+    },
+    { 
+      icon: GroupOutline,
+    },  
+  ]);   
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Friend />
-        </header>
+    <div className="main">
+      <div className="topbar d-flex">
+        {list.map((value, key) => (
+          <a
+            key={key}
+            href="#1"
+            className="d-flex flex-grow-1 justify-content-center align-items-center"
+          >
+            <value.icon />
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
